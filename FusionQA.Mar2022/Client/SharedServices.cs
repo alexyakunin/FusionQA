238384278cf2e9b00976e1ff06023d991c5491a1
Alex Yakunin <alex.yakunin@gmail.com>
2022-03-28 10:35:59 -0700

    feat: show how to add Task<AuthState>, Task<User>, etc. as injected services

--- FusionQA.Mar2022/Client/SharedServices.cs
+++ FusionQA.Mar2022/Client/SharedServices.cs
@@ -1,4 +1,6 @@
-﻿using Stl.Fusion;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Stl.Fusion;
+using Stl.Fusion.Authentication;
 using Stl.Fusion.Blazor;
 using Stl.Fusion.Extensions;
 using Stl.Fusion.UI;
@@ -14,6 +16,23 @@ public static class SharedServices
 
         fusion.AddBlazorUIServices();
         fusion.AddFusionTime();
+
+        services.AddScoped<Task<User>>(async c => {
+            var auth = c.GetRequiredService<IAuth>();
+            var sessionResolver = c.GetRequiredService<ISessionResolver>();
+            var session = await sessionResolver.GetSession(CancellationToken.None).ConfigureAwait(false);
+            var user = await auth.GetUser(session).ConfigureAwait(false);
+            return user;
+        });
+        
+        services.AddScoped<Task<AuthenticationState>>(async c => {
+            var asp = c.GetRequiredService<AuthStateProvider>();
+            return await asp.GetAuthenticationStateAsync().ConfigureAwait(false);
+        });
+        services.AddTransient<Task<AuthState>>(async c => {
+            var asp = c.GetRequiredService<AuthStateProvider>();
+            return (AuthState) await asp.GetAuthenticationStateAsync().ConfigureAwait(false);
+        });
         
         services.AddTransient<IUpdateDelayer>(c => new UpdateDelayer(c.GetRequiredService<IUICommandTracker>(), 0.1)); 
     }
